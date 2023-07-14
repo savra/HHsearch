cb527ea559821dc44bb61c96228d955cf227e48a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-07-14 08:04:13 +0300

    Деплойменты на оставшиееся сервисы

--- hhsearch-startupservice/src/main/java/com/hvdbs/savra/hhsearchstartupservice/HhsearchStartupserviceApplication.java
+++ hhsearch-startupservice/src/main/java/com/hvdbs/savra/hhsearchstartupservice/HhsearchStartupserviceApplication.java
@@ -23,31 +23,5 @@ public class HhsearchStartupserviceApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void run() {
         fileService.readKeywords();
-//        AtomicInteger pageCount = new AtomicInteger(1);
-//        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("search_keywords.txt")) {
-//            if (resourceAsStream != null) {
-//                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceAsStream, StandardCharsets.UTF_8))) {
-//                    Flux.fromStream(bufferedReader.lines())
-//                            .flatMap(keyword ->
-//                                    Flux.fromStream(Stream.iterate(0, page -> page < pageCount.get(), page -> page + 1))
-//                                            .flatMap(page -> searchService.findVacancies(keyword, page, perPage)
-//                                                    .map(vacanciesRs -> {
-//                                                        if (vacanciesRs.getPage() == 0) {
-//                                                            pageCount.set(vacanciesRs.getPages());
-//                                                        }
-//
-//                                                        return vacanciesRs.getItems()
-//                                                                .stream()
-//                                                                .map(VacancyItem::getVacancyId);
-//                                                    })
-//                                                    .flatMapMany(Flux::fromStream)
-//                                                    .take(1)
-//                                                    .map(searchService::findVacancy)
-//                                                    .flatMap(vacancyItem -> vacancyItem.map(vacancyMapper::toEntity)))
-//                            )
-//                            .subscribe(vacancyRepository::save);
-//                }
-//            }
-//        }
     }
 }
