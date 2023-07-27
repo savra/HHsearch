87816ed681ee48b142a25140effafc26cae2b81e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-07-27 20:47:32 +0300

    Добавил service discovery clients

--- hhsearch-service-discovery/src/main/java/com/hvdbs/savra/hhsearchservicediscovery/HhsearchServiceDiscoveryApplication.java
+++ hhsearch-service-discovery/src/main/java/com/hvdbs/savra/hhsearchservicediscovery/HhsearchServiceDiscoveryApplication.java
@@ -2,10 +2,11 @@ package com.hvdbs.savra.hhsearchservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HhsearchServiceDiscoveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HhsearchServiceDiscoveryApplication.class, args);
 	}
