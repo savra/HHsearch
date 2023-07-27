87d22e495b149e77aa2487c41cefbf76b28ed956
Sergey Savrukhin <savra.sv@yandex.ru>
2023-07-27 17:12:22 +0300

    Добавил service discovery

---
+++ hhsearch-service-discovery/src/main/java/com/hvdbs/savra/hhsearchservicediscovery/HhsearchServiceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.hvdbs.savra.hhsearchservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhsearchServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhsearchServiceDiscoveryApplication.class, args);
+	}
+}
---
+++ hhsearch-service-discovery/src/test/java/com/hvdbs/savra/hhsearchservicediscovery/HhsearchServiceDiscoveryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hvdbs.savra.hhsearchservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhsearchServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
