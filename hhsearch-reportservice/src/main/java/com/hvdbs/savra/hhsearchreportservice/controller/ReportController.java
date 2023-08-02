c7ee344ab0bc46a3804df85cb7cba56a7e0364e0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-02 07:43:45 +0300

    Упражнялся с bulkheads. Добавил контроллер, который вызывается из тестового проекта с bulkheads

--- hhsearch-reportservice/src/main/java/com/hvdbs/savra/hhsearchreportservice/controller/ReportController.java
+++ hhsearch-reportservice/src/main/java/com/hvdbs/savra/hhsearchreportservice/controller/ReportController.java
@@ -2,6 +2,7 @@ package com.hvdbs.savra.hhsearchreportservice.controller;
 
 import com.hvdbs.savra.hhsearchreportservice.service.ReportService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/report")
 @RestController
@@ -19,5 +21,19 @@ public class ReportController {
     @GetMapping
     public byte[] report() throws IOException {
         return reportService.getReport();
+    }
+
+    @CrossOrigin
+    @GetMapping("/test")
+    public String report3() {
+        log.info("I here!");
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "HELLO FROM REPORT SERVICE";
     }
 }
