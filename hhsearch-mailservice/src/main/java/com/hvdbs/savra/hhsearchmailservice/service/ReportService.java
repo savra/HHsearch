48bf223b1360d119f14bbc4ed9ca4c1e77322c23
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-03 19:24:03 +0300

    Небольшой рефакторинг

--- hhsearch-mailservice/src/main/java/com/hvdbs/savra/hhsearchmailservice/service/ReportService.java
+++ hhsearch-mailservice/src/main/java/com/hvdbs/savra/hhsearchmailservice/service/ReportService.java
@@ -1,7 +1,18 @@
 package com.hvdbs.savra.hhsearchmailservice.service;
 
 import com.hvdbs.savra.hhsearchmailservice.model.entity.Report;
+import com.hvdbs.savra.hhsearchmailservice.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ReportService {
-    void save(Report report);
+@RequiredArgsConstructor
+@Service
+public class ReportService {
+    private final ReportRepository reportRepository;
+
+    @Transactional
+    public void save(Report report) {
+        reportRepository.save(report);
+    }
 }
